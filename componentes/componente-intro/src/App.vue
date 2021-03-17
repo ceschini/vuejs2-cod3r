b45b02a9693373df50fd65c2e4710fc8daac2778
Lucas Ceschini <ceschini@protonmail.com>
2021-03-17 10:52:54 -0300

    intro componentes

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <app-contadores />
+    <!-- erro, app-contador nao foi declarado -->
+    <!-- <app-contador /> -->
+    <button>botao do app</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
