885a7e41a03e96b64aa048146311d2d05fd3a68e
Lucas Ceschini <ceschini@protonmail.com>
2021-03-19 08:10:05 -0300

    componentes avancado

---
+++ componentes-avancado/citacoes/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <span>
+      <button @click="componente = 'Citacoes'">Citações</button>
+      <button @click="componente = 'Sobre'">Sobre</button>
+    </span>
+    <!-- componente dinamico, criado e destruido toda vez
+    que houver troca, a nao ser que esteja dentro desse 
+    elemento keep-alive, nesse caso vai ser ativado e desativado -->
+    <keep-alive>
+      <component :is="componente" />
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import Citacoes from "./components/Citacoes";
+import Sobre from "./components/Sobre";
+
+export default {
+  components: { Citacoes, Sobre },
+  data() {
+    return {
+      componente: "Citacoes",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 1.6rem;
+}
+
+button {
+  outline: none;
+  padding: 5px 15px 10px;
+  margin: 10px 5px;
+  border-radius: 3px;
+  font-size: 2rem;
+  font-weight: 300;
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
---
+++ componentes-avancado/citacoes/src/components/Citacao.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="citacao">
+    <slot name="fonte"></slot>
+    <slot name="texto"></slot>
+    <slot name="autor"></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+/* eh possivel aplicar estilo no elemento recebido pelo 'slot' 
+mas eh mais fraco que o estilo feito pelo componente que chama */
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
+</style>
